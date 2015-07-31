001cb6958ac6f35c3b913efdcf319f77826120a0
enkiv2 <john.ohno@gmail.com>
2015-07-31 15:28:25 -0500

    break logic into function; increase iterations to six; remove words with apostrophes; capitalize the first letter of each word

--- password.sh
+++ password.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env zsh
-echo  $(cat /usr/share/dict/words | sort -R | head -n 1)$(cat /usr/share/dict/words | sort -R | head -n 1)$(cat /usr/share/dict/words | sort -R | head -n 1)$(cat /usr/share/dict/words | sort -R | head -n 1)
+function rword() {
+	cat /usr/share/dict/words | grep -v "'" | sort -R | head -n 1 
+}
+function capitalize() {
+	echo $(echo $1 | head -c 1 | tr 'a-z' 'A-Z')$(echo $1 | tail -c +2)
+}
+w=""
+for i in 1 2 3 4 5 6 ; do
+	w="$w$(capitalize $(rword))"
+done
+echo  $w
